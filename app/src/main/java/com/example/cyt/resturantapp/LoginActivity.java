161cf8abce702f112422025f10837b9f3c0f5224
cytcool <675149948@qq.com>
2018-06-13 22:53:01 +0800

    登录页实现

--- app/src/main/java/com/example/cyt/resturantapp/LoginActivity.java
+++ app/src/main/java/com/example/cyt/resturantapp/LoginActivity.java
@@ -1,13 +1,62 @@
 package com.example.cyt.resturantapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    
+    private EditText mEtUsername;
+    private EditText mEtPassword;
+    private Button mBtnLogin;
+    private TextView mTvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        initView();
+
+        initEvent();
+    }
+
+    private void initEvent() {
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toOrderActivity();
+            }
+        });
+
+        mTvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toRegisterActivity();
+            }
+        });
+    }
+
+    private void toOrderActivity(){
+        Intent intent = new Intent(this,OrderActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void toRegisterActivity(){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initView() {
+        mEtUsername = findViewById(R.id.id_ed_username);
+        mEtPassword = findViewById(R.id.id_ed_password);
+        mBtnLogin = findViewById(R.id.id_btn_login);
+        mTvRegister = findViewById(R.id.id_tv_register);
     }
 }

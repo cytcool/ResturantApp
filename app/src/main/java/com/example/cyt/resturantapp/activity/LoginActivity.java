efd96f8eea4663e025a8636ac7d77b684a3da727
cytcool <675149948@qq.com>
2018-06-20 22:42:43 +0800

    注册业务逻辑实现

--- app/src/main/java/com/example/cyt/resturantapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/cyt/resturantapp/activity/LoginActivity.java
@@ -1,14 +1,13 @@
 package com.example.cyt.resturantapp.activity;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.cyt.resturantapp.R;
 import com.example.cyt.resturantapp.UserInfoHolder;
@@ -16,6 +15,8 @@ import com.example.cyt.resturantapp.bean.User;
 import com.example.cyt.resturantapp.biz.UserBiz;
 import com.example.cyt.resturantapp.net.CommonCallback;
 import com.example.cyt.resturantapp.utils.T;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.cookie.CookieJarImpl;
 
 public class LoginActivity extends BaseActivity {
     
@@ -26,6 +27,9 @@ public class LoginActivity extends BaseActivity {
 
     private UserBiz mUserBiz;
 
+    private static final String KEY_USERNAME = "key_username";
+    private static final String KEY_PASSWORD = "key_password";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +38,15 @@ public class LoginActivity extends BaseActivity {
         initView();
 
         initEvent();
+
+        initIntent(getIntent());
+
+        mUserBiz = new UserBiz();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     private void initEvent() {
@@ -91,10 +104,41 @@ public class LoginActivity extends BaseActivity {
         finish();
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        
+        initIntent(intent);
+    }
+
+    private void initIntent(Intent intent) {
+        if (intent == null){
+            return;
+        }
+
+        String username = intent.getStringExtra(KEY_USERNAME);
+        String password = intent.getStringExtra(KEY_PASSWORD);
+
+        if (TextUtils.isEmpty(username)||TextUtils.isEmpty(password)){
+            return;
+        }
+
+        mEtUsername.setText(username);
+        mEtPassword.setText(password);
+    }
+
     private void initView() {
         mEtUsername = findViewById(R.id.id_ed_username);
         mEtPassword = findViewById(R.id.id_ed_password);
         mBtnLogin = findViewById(R.id.id_btn_login);
         mTvRegister = findViewById(R.id.id_tv_register);
+    }
+
+    public static void launch(Context context, String username, String password) {
+        Intent intent = new Intent(context,LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.putExtra(KEY_USERNAME,username);
+        intent.putExtra(KEY_PASSWORD,password);
+        context.startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cyt/resturantapp/activity/RegisterActivity.java
+++ app/src/main/java/com/example/cyt/resturantapp/activity/RegisterActivity.java
@@ -2,11 +2,17 @@ package com.example.cyt.resturantapp.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.cyt.resturantapp.R;
+import com.example.cyt.resturantapp.UserInfoHolder;
+import com.example.cyt.resturantapp.bean.User;
+import com.example.cyt.resturantapp.biz.UserBiz;
+import com.example.cyt.resturantapp.net.CommonCallback;
+import com.example.cyt.resturantapp.utils.T;
 
 
 public class RegisterActivity extends BaseActivity {
@@ -17,16 +23,20 @@ public class RegisterActivity extends BaseActivity {
     private Button mBtnRegister;
     private Toolbar toolbar;
 
+    private UserBiz mUserBiz;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        setUpToolbar();
+        //setUpToolbar();
         initView();
         initEvent();
 
         setTitle("注册");
+
+        mUserBiz = new UserBiz();
     }
 
 
@@ -34,7 +44,38 @@ public class RegisterActivity extends BaseActivity {
         mBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String username = mEtUsername.getText().toString();
+                String password = mEtPassword.getText().toString();
+                String repassword = mEtRePassword.getText().toString();
 
+                if (TextUtils.isEmpty(username)||TextUtils.isEmpty(password)){
+                    T.showToast("账号或者密码不能为空");
+                    return;
+                }
+
+                if (!password.equals(repassword)){
+                    T.showToast("两次输入的密码不一致");
+                }
+
+                startLoadingProgress();
+
+                mUserBiz.register(username, password, new CommonCallback<User>() {
+                    @Override
+                    public void onError(Exception e) {
+                        stopLoadingProgress();
+                        T.showToast(e.getMessage());
+                    }
+
+                    @Override
+                    public void onSuccess(User response) {
+                        stopLoadingProgress();
+                        T.showToast("注册成功,用户名为："+response.getUsername());
+
+                        LoginActivity.launch(RegisterActivity.this,response.getUsername(),response.getPassword());
+                        finish();
+                    }
+
+                });
             }
         });
     }
@@ -44,5 +85,16 @@ public class RegisterActivity extends BaseActivity {
         mEtPassword = findViewById(R.id.id_et_password);
         mEtRePassword = findViewById(R.id.id_et_rePassword);
         mBtnRegister = findViewById(R.id.id_btn_register);
+    }
+
+    protected void setUpToolbar(){
+        Toolbar toolbar = findViewById(R.id.id_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
